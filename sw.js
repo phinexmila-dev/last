03b0fadbad7662e0623188a331b865a8e7930bbb
phinexmila-dev <phinexmila@gmail.com>
2026-05-21 18:14:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
